c3fb0d6f0840ae0b1989a09fca89594f9ddb160a
absar <absarbhu854326@gmail.com>
2025-11-02 19:10:42 +0530

    Money Manager Project's Backend Part is initialized

---
+++ Money-Manager-System/src/main/java/com/saar/mms/MoneyManagerSystemApplication.java
@@ -0,0 +1,22 @@
+package com.saar.mms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class MoneyManagerSystemApplication {
+
+	
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+		SpringApplication.run(MoneyManagerSystemApplication.class, args);
+	}
+
+}
---
+++ Money-Manager-System/src/test/java/com/saar/mms/MoneyManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saar.mms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
